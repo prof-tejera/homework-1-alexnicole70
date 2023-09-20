8fa8c59b9f403da401cb4f4cb0cc80795da03428
Alex <51425004+alexnicole70@users.noreply.github.com>
2023-09-19 21:36:13 -0400

    Finished Homework 1

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 // Recommend using node-fetch for those familiar with JS fetch
 
-const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
+const COLORS = "https://nt-cdn.s3.amazonaws.com/colors.json";
 
 /**
  * @param name filter for color name
@@ -10,8 +10,51 @@ const COLORS = 'https://nt-cdn.s3.amazonaws.com/colors.json';
  * @param compHex filter for complementary color hex code
  * @returns Promise
  */
+
 const fetchColors = ({ name, hex, compName, compHex }) => {
-  throw Error('Not implemented');
+  return new Promise((resolve, reject) => {
+    fetch(COLORS)
+      .then((v) => v.json())
+      .then((colorData) => {
+        // name contains string (case insensitive)
+        if (name) {
+          resolve(
+            colorData.filter((color) => color.name.toLowerCase().includes(name.toLowerCase()))
+          );
+        }
+        // hex code (without '#')
+        if (hex) {
+          resolve(
+            colorData.filter((inputColor) => inputColor.hex.includes(hex))
+          );
+        }
+
+        // it contains a complementary color whose name contains a string (case insensitive)
+        if (compName) {
+          resolve(
+            colorData.filter(
+              (inputColor) =>
+                inputColor.comp.filter((inputCompColor) =>
+                  inputCompColor.name.toLowerCase().includes(compName.toLowerCase())
+                ).length > 0
+            )
+          );
+        }
+
+        // it contains a complementary color whose hex code equals a given hex code (without '#')
+
+        if (compHex) {
+          resolve(
+            colorData.filter(
+              (inputColor) =>
+                inputColor.comp.filter((inputCompColor) =>
+                  inputCompColor.hex.includes(compHex)
+                ).length > 0
+            )
+          );
+        }
+      });
+  });
 };
 
 // Leave this here
